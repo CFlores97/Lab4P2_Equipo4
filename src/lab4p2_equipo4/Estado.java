acf548edb3c45ffbc0f2de45fda9f578e12f19f1
Harold Camas <haroldcamas99@gmail.com>
2023-08-11 14:14:57 -0600

    Movimiento y estado

---
+++ src/lab4p2_equipo4/Estado.java
@@ -0,0 +1,36 @@
+package lab4p2_equipo4;
+import java.util.Random;
+
+public class Estado extends Movimiento{
+    private String estado;
+
+    public Estado() {
+    }
+
+    public Estado(String estado, String nombre, String descripcion) {
+        super(nombre, descripcion);
+        this.estado = estado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Estado{" + "estado=" + estado + '}';
+    }
+
+    @Override
+    public void Ataque(Pokemon atacante, Pokemon defensor) {
+        Random rand = new Random();
+        int rng = rand.nextInt(100) + 1;
+        if(rng < 76){
+            
+        }
+    }
+}
